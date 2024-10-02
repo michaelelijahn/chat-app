d89a9a021ca7441d1698068e6c0506f469a6abe6
Michael <natawidjajamichael@gmail.com>
2024-10-02 22:20:27 +1000

    setup prisma schema and model

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
-import express from "express"
+import express from "express";
+import authenticationRoutes from "./routes/auth.route.js";
+import chatRoutes from "./routes/chat.route.js"
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Testing ");
-});
+app.use("/api/auth", authenticationRoutes);
+app.use("/api/chats", chatRoutes);
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3000");
 });
